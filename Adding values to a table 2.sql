fbba7836e8e9a593e93aff1e71fba74cfc34a195
Artayob <artayob786@gmail.com>
2025-05-30 16:11:43 +0200

    Adding my work so i dont loose it

---
+++ Adding values to a table 2.sql
@@ -0,0 +1,8 @@
+insert into Luxury(Car_Name, Car_Power, Car_price)
+--          values('Mercedes-Benz S-Class S-500', 320, 2500000)
+--          values('BMW 7 Series', 390, 2400000)
+--          values('Audi A8', 340, 2300000)
+--          values('Jaguar F-Pace', 300, 1400000 )
+--          values('Volkswagen Golf R ', 235, 945100)
+--          values('Audi A6', 183, 1184100)
+            values('Bmw 520d', 223, 1248731)
---
+++ Creating Tables and databases.sql
@@ -0,0 +1,26 @@
+create database CAR_CHOICE
+create table Budget(
+Car_Name varchar(255), 
+Car_Power int,
+Car_price int
+)
+create table Luxury(
+Car_name varchar(255),
+Car_Power int,
+Car_price int
+)
+create table Sport(
+Car_name varchar(255),
+Car_Power int,
+Car_price int
+)
+create table HyperSport(
+Car_name varchar(255),
+Car_Power int,
+Car_price int
+)
+create table SUV(
+Car_name varchar(255),
+Car_Power int,
+Car_price int
+)
